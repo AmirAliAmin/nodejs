12639ca1148b8caabdce562e20f266cbba2fb0de
AmirAliAmin <aliaminamir77@gmail.com>
2025-07-24 00:01:30 +0500

    middlewaser

--- Project01/index.js
+++ Project01/index.js
@@ -6,7 +6,20 @@ const users = require("./MOCK_DATA.json")
 const app = express()
 const port = 8000
 app.use(express.urlencoded({extended:false}))
+app.use((req,res,next)=>{
+    console.log("MiddleWare 1");
+    fs.appendFile("log.txt", `\n ${Date.now()}, ${req.ip} ${req.method}: ${req.path}`, (errr,data)=>{
+        next()
 
+    })
+    // req.myName ="Amir Ali"
+    // res.end("hey")
+})
+app.use((req,res,next)=>{
+    console.log("MiddleWare 2");
+    // res.end("Amir")
+    next();
+})
 //Routes
 app.get("/users", (req,res)=>{
     const html = `
@@ -19,21 +32,31 @@ app.get("/users", (req,res)=>{
 
 //get
 app.get("/api/users", (req,res)=>{
+    res.setHeader("X-myName", "Amir Ali")  //custom header
+    //always add X in your custom header
+    // console.log(req.headers)
+    // console.log(req.myName)
     return res.json(users)
 })
 
 app.get("/api/users/:id", (req,res)=>{
     const id = Number(req.params.id)
     const user = users.find((user)=>user.id === id);
+    if(!user) {
+        return res.status(404).json({ status: "fail", message: "User not found" });
+    }
     return res.json(user)
 })
 
 app.post("/api/users", (req,res)=>{
     const body = req.body;
+    if(!body ||!body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+       return res.status(400).json({msg:"fill all the field"})
+    }
     // console.log("Body", body)
     users.push({...body, id: users.length+1})
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data)=>{
-        return res.json({status: "success", id: users.length})
+        return res.status(202).json({status: "success", id: users.length})
     })
     //todo:create new user
 })
